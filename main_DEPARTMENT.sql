6540766c6b3dcfb522dfa22e4f982e298014fecf
Zach Baruch <zbaruch20@gmail.com>
2021-03-09 15:08:39 -0500

    Removed old files

--- main_DEPARTMENT.sql
+++
@@ -1,4 +0,0 @@
-insert into main.DEPARTMENT (Dname, Dnumber, Mgr_ssn, Mgr_start_date)
-values  ('Headquarters', 1, '888665555', '1981-06-19'),
-        ('Administration', 4, '987654321', '1995-01-01'),
-        ('Research', 5, '333445555', '1988-05-22');
--- main_DEPT_LOCATIONS.sql
+++
@@ -1,6 +0,0 @@
-insert into main.DEPT_LOCATIONS (Dnumber, Dlocation)
-values  (1, 'Houston'),
-        (4, 'Stratford'),
-        (5, 'Bellaire'),
-        (5, 'Sugarland'),
-        (5, 'Houston');
